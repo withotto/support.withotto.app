db22df20017b9efcd23082088124c09c06dcc837
Stuart Chapman <StuBez@users.noreply.github.com>
2024-10-04 13:23:56 +0100

    Initial version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,65 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [
+        starlight({
+            customCss: [
+                "./src/style.css",
+            ],
+            title: 'With Otto support',
+            sidebar: [{
+                label: 'Getting started',
+                items: [{
+                    label: 'Onboarding',
+                    slug: 'getting-started/onboarding'
+                }, {
+                    label: 'Making the most of your trial',
+                    slug: 'getting-started/making-the-most-of-your-trial'
+                }, {
+                    label: 'Giving Otto access to your clients',
+                    slug: 'getting-started/giving-otto-access-to-your-clients'
+                }]
+            }, {
+                label: 'Guides',
+                items: [{
+                    label: 'SmartMatch',
+                    slug: 'guides/smartmatch'
+                }, {
+                    label: 'GuidedMatch',
+                    slug: 'guides/guidedmatch'
+                }, {
+                    label: 'Feedback',
+                    slug: 'guides/feedback'
+                }, {
+                    label: 'What can be reconciled?',
+                    slug: 'guides/what-can-be-reconciled'
+                }, {
+                    label: 'How does Otto decide what to reconcile?',
+                    slug: 'guides/how-does-otto-decide-what-to-reconcile'
+                }]
+            }, {
+                label: 'The portal',
+                items: [{
+                    label: 'Dashboard',
+                    slug: 'portal/dashboard'
+                }, {
+                    label: 'Clients',
+                    slug: 'portal/clients'
+                }, {
+                    label: 'Settings',
+                    slug: 'portal/settings'
+                }, {
+                    label: 'Billing',
+                    slug: 'portal/billing'
+                }]
+            }]
+        }),
+        tailwind({
+            applyBaseStyles: false,
+        })
+    ]
 });
